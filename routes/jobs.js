9130ff1129061ff8cb74c6bcfd8a27d6c47becf2
Marco Santana <marcos.maio96@gmail.com>
2021-03-05 16:27:48 -0300

    add project job finder

---
+++ routes/jobs.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+const Job = require("../models/Job");
+
+// Rota de teste
+router.get("/test", (req, res) => {
+    res.send("deu certo");
+});
+
+// Detalhe da vaga
+router.get("/view/:id", (req, res) => Job.findOne({
+where: {id: req.params.id}
+}).then(job => {
+    
+    res.render("view", {
+        job
+    });
+
+}).catch(err => console.log(err)));
+
+
+// Form da rota de envio
+router.get("/add", (req, res) => {
+    res.render("add");
+});
+
+// Add job via post
+router.post("/add", (req, res) => {
+
+    let {title, salary, company, description, email, new_job} = req.body;
+
+    // insert = inserir dados no sistema
+    Job.create({
+        title,
+        description,
+        salary,
+        company,
+        email,
+        new_job
+    })
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err));
+
+});
+
+module.exports = router;
